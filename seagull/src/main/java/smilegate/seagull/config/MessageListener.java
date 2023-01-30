2d3ee092d79f150e131b0ede9735a3845b9e4d68
tjdwns4537 <tjdwns4537@naver.com>
2023-01-31 02:55:46 +0900

    feat: 프로젝트 생성 및 카프카 기능 구현

---
+++ seagull/src/main/java/smilegate/seagull/config/MessageListener.java
@@ -0,0 +1,26 @@
+package smilegate.seagull.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+import smilegate.seagull.model.Message;
+import smilegate.seagull.util.KafkaConstants;
+
+@Slf4j
+@Component
+public class MessageListener {
+
+    @Autowired
+    SimpMessagingTemplate template;
+
+    @KafkaListener(
+            topics = KafkaConstants.KAFKA_TOPIC,
+            groupId = KafkaConstants.GROUP_ID
+    )
+    public void listen(Message message) {
+        log.info("sending via kafka listener..");
+        template.convertAndSend("/topic/group", message);
+    }
+}
