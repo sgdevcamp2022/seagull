7ad4c56c4e655b805fcd2917b6aeee88ae2e41af
tjdwns4537 <tjdwns4537@naver.com>
2023-02-02 01:54:00 +0900

    feat: websocket,kafka 채팅 웹, 백엔드 작업 테스트 완료

--- seagull/src/main/java/smilegate/seagull/config/MessageListener.java
+++ seagull/src/main/java/smilegate/seagull/config/MessageListener.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
-import smilegate.seagull.model.Message;
+import smilegate.seagull.model.ChatMessage;
 import smilegate.seagull.util.KafkaConstants;
 
 @Slf4j
@@ -19,8 +19,8 @@ public class MessageListener {
             topics = KafkaConstants.KAFKA_TOPIC,
             groupId = KafkaConstants.GROUP_ID
     )
-    public void listen(Message message) {
+    public void listen(ChatMessage chatMessage) {
         log.info("sending via kafka listener..");
-        template.convertAndSend("/topic/group", message);
+        template.convertAndSend("/topic/group", chatMessage);
     }
 }
