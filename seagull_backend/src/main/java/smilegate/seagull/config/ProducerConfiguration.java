749e0f6acf22f3fa26aaae826c90d9f12597664e
tjdwns4537 <tjdwns4537@naver.com>
2023-02-06 21:48:22 +0900

    chore: rebase not conflict

--- seagull_backend/src/main/java/smilegate/seagull/config/ProducerConfiguration.java
+++ seagull_backend/src/main/java/smilegate/seagull/config/ProducerConfiguration.java
@@ -23,7 +23,7 @@ import java.util.Properties;
 public class ProducerConfiguration {
 
     @Value("${kafka.broker}")
-    public String KAFKA_BROKER; // 생성한 토픽의 이름
+    public String KAFKA_BROKER; // 카프카 서버 주소
 
     @Bean
     public KafkaTemplate<String, ChatMessage> kafkaTemplate() {
@@ -38,8 +38,8 @@ public class ProducerConfiguration {
     @Bean
     public Map<String, Object> producerConfigurations() {
         Map<String, Object> configurations = new HashMap<>(); // 카프카 프로듀서 객체 생성
-        configurations.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_BROKER);
-        configurations.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configurations.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_BROKER); // 카프카 실행 주소
+        configurations.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class); // 데이터,키 직렬화
         configurations.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return configurations;
     }
