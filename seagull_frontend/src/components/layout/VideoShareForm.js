8f497cf391d1f06ae7d4590893265101962b830e
jjjjjeongwon <jjjjjwon21@gmail.com>
2023-02-15 02:14:15 +0900

    feat: 영상공유, 채팅 기능 및 로그인 요청페이지 구현

--- seagull_frontend/src/components/layout/VideoShareForm.js
+++ seagull_frontend/src/components/layout/VideoShareForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactPlayer from 'react-player';
 import styled from 'styled-components';
 
 const VideoShareForm = () => {
@@ -12,8 +13,6 @@ const VideoShareForm = () => {
 const Wrap = styled.div`
   width: 100%;
   height: calc(100vh - 120px);
-  /* background-color: aqua; */
-  /* margin-top: 5px; */
   display: flex;
   align-items: flex-start;
   justify-content: center;
@@ -22,7 +21,6 @@ const Wrap = styled.div`
 const VideoWrap = styled.div`
   width: 90%;
   height: calc(100vh - 120px);
-  /* border: 1px solid gray; */
   background-color: white;
 `;
 
