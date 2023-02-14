a5c4cf1ef908209b3197af79d9b5c9789d9a0e06
tjdwns4537 <tjdwns4537@naver.com>
2023-02-14 22:14:08 +0900

    feat: RoomController 기능추가

--- seagull_backend/src/main/java/smilegate/seagull/room/controller/RoomController.java
+++ seagull_backend/src/main/java/smilegate/seagull/room/controller/RoomController.java
@@ -2,10 +2,7 @@ package smilegate.seagull.room.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -18,8 +15,10 @@ import smilegate.seagull.room.domain.RoomUser;
 import smilegate.seagull.room.service.EnterUserService;
 import smilegate.seagull.room.service.RoomService;
 
+import java.net.http.HttpResponse;
 import java.nio.charset.Charset;
 import java.util.Optional;
+import java.util.Set;
 
 @Slf4j
 @Controller
@@ -76,11 +75,27 @@ public class RoomController {
     }
 
     @PostMapping("{roomLink}")
-    public ResponseEntity<?> enterRoom(@PathVariable(value = "roomLink") String roomLink, @RequestBody RoomUser roomUser) {
+    public ResponseEntity<HttpHeaders> enterRoom(@PathVariable(value = "roomLink") String roomLink, @RequestBody RoomUser roomUser) {
         enterUserService.enter(roomUser);
-
         HttpHeaders headers= new HttpHeaders();
         headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
         return new ResponseEntity<>(headers, HttpStatus.OK);
+    }
+
+    @GetMapping("/list/{roomLink}")
+    public ResponseEntity<Set<String>> getRoomList(@PathVariable(value = "roomLink") String roomLink) {
+        Set<String> allUser = enterUserService.getAllUser(roomLink);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+        if(!allUser.isEmpty()) return new ResponseEntity<>(allUser, headers, HttpStatus.OK);
+        return new ResponseEntity<>(headers, HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/count/{roomLink}")
+    public ResponseEntity<Integer> getRoomCount(@PathVariable(value = "roomLink") String roomLink) {
+        Integer userNum = enterUserService.countUser(roomLink);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+        return new ResponseEntity<>(userNum, headers, HttpStatus.NOT_FOUND);
     }
 }
