b9c58fdb72839c43edbacebc69db2fbb9fbc10d3
tjdwns4537 <tjdwns4537@naver.com>
2023-02-16 23:22:55 +0900

    feat: 영상 url 저장 및 재생 중간에 들어온 유저 관리 기능 추가

---
+++ seagull_backend/src/main/java/smilegate/seagull/room/service/VideoService.java
@@ -0,0 +1,32 @@
+package smilegate.seagull.room.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import smilegate.seagull.room.repository.RoomVideoRepository;
+
+@Service
+public class VideoService {
+
+    private final RoomVideoRepository roomVideoRepository;
+
+    @Autowired
+    public VideoService(RoomVideoRepository roomVideoRepository) {
+        this.roomVideoRepository = roomVideoRepository;
+    }
+
+    public void saveUrl(String roomLink, String url){
+        roomVideoRepository.setURL(roomLink,url);
+    }
+
+    public void deleteUrl(String roomLink, String url){
+        roomVideoRepository.deleteUrl(roomLink,url);
+    }
+
+    public void deleteUrlAll(String roomLink) {
+        roomVideoRepository.deleteUrlAll(roomLink);
+    }
+
+    public String getURL(String roomLink) {
+        return roomVideoRepository.findByRoomLink(roomLink);
+    }
+}
